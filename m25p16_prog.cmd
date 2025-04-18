d5f3fc0e371e8e8f91b70efaea4f2cc8e2621c10
sgd <sgot@inbox.ru>
2025-04-18 15:01:51 +0300

    Update: cmd script.

--- m25p16_prog.cmd
+++ m25p16_prog.cmd
@@ -1,5 +1,29 @@
 @ECHO OFF
 hex2bin -c top.mcs
-spi_flash --chip M25P16 --write top.bin --clk 1000000 --channel 0
-spi_flash --chip M25P16 --cmp top.bin --clk 1000000 --offset 0 --channel 0
 
+spi_flash --chip M25P16 --write top.bin --clk 1000000 --channel 0
+if %ErrorLevel% equ 0 goto compare
+goto error_write
+
+:compare
+spi_flash --chip M25P16 --cmp top.bin --clk 1000000 --offset 0 --channel 0
+if %ErrorLevel% equ 0 goto exit
+goto error_compare
+
+:exit
+echo "================================================================================"
+echo "---- OK ----"
+echo "================================================================================"
+exit 0
+
+:error_write
+echo "================================================================================"
+echo "---- ERROR WRITE ----"
+echo "================================================================================"
+exit 1
+
+:error_compare
+echo "================================================================================"
+echo "---- ERROR COMPARE ----"
+echo "================================================================================"
+exit 1
